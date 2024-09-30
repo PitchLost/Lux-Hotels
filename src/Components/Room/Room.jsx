1cbfc7c26e29a3b3a9a8beb4285e5964e548947c
PitchLost <20067ja@kuranuicollege.school.nz>
2024-09-30 13:50:11 +1300

    Add a ribbon for tags

--- src/Components/Room/Room.jsx
+++ src/Components/Room/Room.jsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Room.css'
 
 // Import components
 import Tags from './Tags/Tags'
 import Rooms from './Rooms/Rooms'
 
+import { DarkContext } from '../../App'
+
 
 function Room() {
+    const [darkMode ,setDarkMode] = useContext(DarkContext)
+    const roomClass = darkMode ? 'room-dark': 'room-light'
     const [selectedTag, setSelectedTag] = useState({label: 'Premium', filter: 'prem'}) // Selected Tag state for room filtering
 
     // Content
   return (
     <div className='room'>
-      <h1 className='room-main-header'>View our available rooms</h1>
+      <div className={`tags-ribbon ${roomClass}`}>
+
+      </div>
       <div className='room-tags'> 
         <Tags selectedTag={selectedTag} setSelectedTag={setSelectedTag} />
         <h1 className='room-tag-header'>{selectedTag.label}</h1>
